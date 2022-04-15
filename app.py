febf48164fb5f7e94e1d6f5c108df81bdbbe8065
mcfralish <mcfralish@gmail.com>
2022-04-15 02:02:51 -0400

    added app routes for reder pages

--- app.py
+++ app.py
@@ -33,8 +33,80 @@ with app.app_context():
 
 @app.route("/")
 def index():
+    """Renders index page"""
+    return render_template("index.html")
+
+
+@app.route("/a_login")
+def a_login():
+    """Renders login page"""
+    return render_template("a_login.html")
+
+
+@app.route("/admin")
+def admin():
     """Function builds/render web app"""
     return render_template("admin.html")
+
+
+@app.route("/empl_form")
+def empl_form():
+    """Function builds/render web app"""
+    return render_template("empl_form.html")
+
+
+@app.route("/guest_form")
+def guest_form():
+    """Function builds/render web app"""
+    return render_template("guest_form.html")
+
+
+@app.route("/m_login")
+def m_login():
+    """Function builds/render web app"""
+    return render_template("m_login.html")
+
+
+@app.route("/management")
+def management():
+    """Function builds/render web app"""
+    return render_template("management.html")
+
+
+@app.route("/med_form")
+def med_form():
+    """Function builds/render web app"""
+    return render_template("med_form.html")
+
+
+@app.route("/p_login")
+def p_login():
+    """Function builds/render web app"""
+    return render_template("p_login.html")
+
+
+@app.route("/patient_form")
+def patient_form():
+    """Function builds/render web app"""
+    return render_template("patient_form.html")
+
+
+@app.route("/patient")
+def patient():
+    """Function builds/render web app"""
+    return render_template("patient.html")
+
+
+@app.route("/rate")
+def rate():
+    """Function builds/render web app"""
+    return render_template("rate.html")
+
+
+@app.route("/task")
+def task():
+    """Function builds/render web app"""
+    return render_template("task.html")
 
 
 if __name__ == "__main__":
