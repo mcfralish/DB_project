0c3f03a2d954b0b7d61450454ef3c8ab96099613
mcfralish <mcfralish@gmail.com>
2022-04-15 20:46:32 -0400

    big update from night of 04/14/22

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-import datetime
-from typing_extensions import Self
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import *
@@ -28,72 +26,71 @@ class Users(db.Model, UserMixin, Base):
 
 class Department(db.Model, Base):
     dept_no = db.Column(db.Integer, primary_key=True)
-    dep_name = db.Column(db.String(200))
-    building = db.Column(db.String(200))
-    floor = db.Column(db.Integer)
+    dept_name = db.Column(db.String(200), nullable=False)
+    building = db.Column(db.String(200), nullable=False)
+    floor = db.Column(db.Integer, nullable=False)
     assigned_employees = relationship("Employee")
     assigned_patients = relationship("Patient")
 
 
 class Shift(db.Model, Base):
-    shift_id = db.Column(db.Integer, primary_key=True)
-    shift_start = db.Column(db.DateTime)
-    shift_end = db.Column(db.DateTime)
-    work_days = db.Column(db.String(11))
-    assigned_employees = relationship("Employee")
+    shift_no = db.Column(db.Integer, primary_key=True)
+    work_days = db.Column(db.String(200), nullable=False)
+    work_hours = db.Column(db.String(200), nullable=False)
 
 
 class Certification(db.Model, Base):
-    cert_id = db.Column(db.Integer, primary_key=True)
-    cert_name = db.Column(db.String(5))
-    pay = db.Column(db.Float)
+    cert_no = db.Column(db.Integer, primary_key=True)
+    cert_name = db.Column(db.String(5), nullable=False)
+    pay = db.Column(db.Float, nullable=False)
     qualified_emloyees = relationship("Employee")
     qualified_tasks = relationship("Task")
 
 
 class Employee(db.Model, Base):
-    empl_id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(1))
-    first_name = db.Column(db.String(200))
-    last_name = db.Column(db.String(200))
-    phone = db.Column(db.Integer)
-    dob = db.Column(db.DateTime)
-    gender = db.Column(db.String(1))
-    hire = db.Column(db.DateTime)
-    login_id = db.Column(Integer, ForeignKey(Users.id))
-    dept_no = db.Column(db.Integer, ForeignKey(Department.dept_no))
-    shift_no = db.Column(db.Integer, ForeignKey(Shift.shift_id))
-    cert_id = db.Column(db.Integer, ForeignKey(Certification.cert_id))
+    empl_no = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(1), nullable=False)
+    first_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    dob = db.Column(db.String(200), nullable=False)
+    gender = db.Column(db.String(1), nullable=False)
+    hire = db.Column(db.String(200), nullable=False)
+    login_id = db.Column(Integer, ForeignKey(Users.id), nullable=False)
+    dept_no = db.Column(db.Integer, ForeignKey(Department.dept_no), nullable=False)
+    shift_no = db.Column(db.Integer, ForeignKey(Shift.shift_no))
+    cert_no = db.Column(db.Integer, ForeignKey(Certification.cert_no), nullable=False)
 
 
 class Patient(db.Model, Base):
-    patient_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(200))
-    last_name = db.Column(db.String(200))
-    phone = db.Column(db.Integer)
-    dob = db.Column(db.DateTime)
-    gender = db.Column(db.String(1))
-    login_id = db.Column(Integer, ForeignKey(Users.id))
-    caretaker_id = db.Column(db.Integer, ForeignKey(Employee.empl_id))
-    dept_no = db.Column(db.Integer, ForeignKey(Department.dept_no))
-    visitors = relationship("Guest")
+    patient_no = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    dob = db.Column(db.String(200), nullable=False)
+    gender = db.Column(db.String(1), nullable=False)
+    admission_date = db.Column(db.String(200), nullable=False)
+    login_id = db.Column(Integer, ForeignKey(Users.id), nullable=False)
+    dept_no = db.Column(db.Integer, ForeignKey(Department.dept_no), nullable=False)
+    caretaker_no = db.Column(db.Integer, ForeignKey(Employee.empl_no), nullable=False)
+    visitors = relationship("Visitor")
 
 
-class Guest(db.Model, Base):
-    guest_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(200))
-    last_name = db.Column(db.String(200))
-    association = db.Column(db.String(200))
-    visiting_pt = db.Column(db.Integer, ForeignKey(Patient.patient_id))
+class Visitor(db.Model, Base):
+    visitor_no = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(200), nullable=False)
+    association = db.Column(db.String(200), nullable=False)
+    visiting_pt = db.Column(db.Integer, ForeignKey(Patient.patient_no), nullable=False)
 
 
 class Task(db.Model, Base):
-    task_id = db.Column(db.Integer, primary_key=True)
-    required_cert = db.Column(db.Integer, ForeignKey(Certification.cert_id))
-    task_name = db.Column(db.String(200))
-    priority = db.Column(db.Integer)
-    duration = db.Column(db.Integer)
-    required = db.Column(db.Boolean)
+    task_no = db.Column(db.Integer, primary_key=True)
+    required_cert = db.Column(db.Integer, ForeignKey(Certification.cert_no))
+    task_name = db.Column(db.String(200), nullable=False)
+    priority = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    required = db.Column(db.Boolean, nullable=False)
+    isMedicine = db.Column(db.Boolean, nullable=False)
     recurring = db.Column(db.Boolean)
     frequency = db.Column(db.Integer)
-    isMedicine = db.Column(db.Boolean)
