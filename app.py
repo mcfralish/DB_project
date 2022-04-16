59254ea1cd00cdbae02166759b753b46ad005a42
root <root@FrayLaptop.localdomain>
2022-04-16 18:12:30 -0400

    fixed error in run app. Should deploy now.

--- app.py
+++ app.py
@@ -408,4 +408,4 @@ def patient():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=8080)
+    app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
